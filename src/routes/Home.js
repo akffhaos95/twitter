eca515d8615d362e06ccec06b1a3d24ebec58b07
a0104 <akffhaos95@naver.com>
2021-02-08 11:26:15 +0900

    1.5 file upload

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,12 @@
 import Tweet from "components/Tweet";
-import { dbService } from "fbase";
+import { dbService, storageService } from "fbase";
 import React, { useEffect, useState } from "react";
-
+import { v4 as uuidv4 } from 'uuid';
 
 const Home = ({ userObj }) => {
     const [tweet, setTweet] = useState("");
     const [tweets, setTweets] = useState([]);
-
+    const [attachment, setAttachment] = useState();
 
     useEffect(() => {
         dbService.collection("tweets").onSnapshot(snapshot => {
@@ -20,12 +20,22 @@ const Home = ({ userObj }) => {
 
     const onSubmit = async (event) => {
         event.preventDefault();
-        await dbService.collection("tweets").add({
+        let attachmentUrl = "";
+
+        if (attachment !== "") {
+            const attachmentRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
+            const response = await attachmentRef.putString(attachment, "data_url");
+            attachmentUrl = await response.ref.getDownloadURL();
+        }
+        const tweetObj = {
             text: tweet,
             createdAt: Date.now(),
             creatorId: userObj.uid,
-        });
-        setTweet("")
+            attachmentUrl,
+        }
+        await dbService.collection("tweets").add(tweetObj);
+        setTweet("");
+        setAttachment("");
     };
     const onChange = (event) => {
         const {
@@ -33,6 +43,22 @@ const Home = ({ userObj }) => {
         } = event;
         setTweet(value);
     }
+    const onFileChange = (event) => {
+        const {
+            target: {files},
+        } = event;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            const {
+                currentTarget: { result }, 
+            } = finishedEvent;
+            setAttachment(result)
+        }
+        reader.readAsDataURL(theFile);
+
+    };
+    const onClearAttachment = () => setAttachment(null)
     return (
     <div>
         <form onSubmit={onSubmit}>
@@ -43,10 +69,25 @@ const Home = ({ userObj }) => {
                 placeholder="What's on your mind?" 
                 maxLength={120}
             />
+            <input
+                type="file" 
+                accept="image/*"
+                onChange={onFileChange}
+            />
             <input 
                 type="submit" 
                 value="tweet" 
             />
+            {attachment && (
+                <div>
+                    <img 
+                        src={attachment}
+                        width="50px"
+                        height="50px"
+                    />
+                    <button onClick={onClearAttachment}>Clear</button>
+                </div>
+            )}
         </form>
         <div>
             {tweets.map((tweet) => (
